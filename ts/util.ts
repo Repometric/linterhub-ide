009dfdc7f3a0dcd355131c6b401a88b158bba4dc
Oleg Safonov <by.xsofa@gmail.com>
2017-01-27 14:25:21 +0300

    Change the language on typescript

--- ts/util.ts
+++ ts/util.ts
@@ -1,31 +1,31 @@
-"use strict";
-const cp = require("child_process");
+import * as cp from 'child_process';
 
-function executeChildProcess(command, workingDirectory = null) {
+export function executeChildProcess(command: string, workingDirectory: string = null): Promise<string> {
+    // TODO: Return ChildProcess in order to stop it when needed
     let promise = new Promise((resolve, reject) => {
+        // TODO: Use spawn and buffers.
         cp.exec(command, { cwd: workingDirectory, maxBuffer: 1024 * 1024 * 500 }, function (error, stdout, stderr) {
             let execError = stderr.toString();
             if (error) {
                 reject(new Error(error.message));
-            }
-            else if (execError !== '') {
+            } else if (execError !== '') {
                 reject(new Error(execError));
-            }
-            else {
+            } else {
                 resolve(stdout);
             }
         });
     });
+
     return promise;
 }
-exports.executeChildProcess = executeChildProcess;
 
-class Cacheable {
-    constructor(action) {
-        this.value = null;
+export class Cacheable {
+    private value: {} = null;
+    private action: () => Promise<{}>;
+    constructor(action: () => Promise<{}>) {
         this.action = action;
     }
-    getValue() {
+    getValue(): Promise<{}> {
         let that = this;
         let promise = new Promise((resolve, reject) => {
             if (that.value == null) {
@@ -33,12 +33,10 @@ class Cacheable {
                     that.value = value;
                     resolve(that.value);
                 }).catch(error => reject(error));
-            }
-            else {
+            } else {
                 resolve(that.value);
             }
         });
         return promise;
     }
 }
-exports.Cacheable = Cacheable;
