2abce77a27105091434dc3f167925ddc41cb277c
Oleg Safonov <by.xsofa@gmail.com>
2017-08-27 00:19:46 +0300

    Export more classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Linterhub } from './linterhub';
 export { Mode, Logger, Integration } from './types/integration';
-export { LinterhubVersion, Component, Error, Engine, DetectedEngine, DetectType } from './types/linterhub';
+export { LinterhubVersion, Component, Error, Engine, DetectedEngine, DetectType, EngineResult, AnalyzeResult, AnalyzeMessage } from './types/linterhub';
 export { ProgressManager } from './progress';
